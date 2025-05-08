990bd2b3b681531e423a89af763da302929a990a
henry-israel <henryisrael08@gmail.com>
2025-05-08 16:13:31 +0100

    submit script

--- compare_posteriors.C
+++ compare_posteriors.C
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <TTree.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TGraph.h>
+#include <THStack.h>
+#include <TMath.h>
+
+
 TTree* get_posterior_tree(TString file_name){
   TFile* file = TFile::Open(file_name);
   if(file->IsZombie()){
@@ -12,16 +26,64 @@ TTree* get_posterior_tree(TString file_name){
 }
 
 
-TH1D* get_hist_from_ttree(TTree* tree, TString branch_name, TString tree_label, int nbins, double min_val, double max_val){
+TH1D* fix_hist(TTree* tree, TString branch_name, TString tree_label, double min_val, double max_val, int nbins, int step_cut=100000){
+  TH1D* h = new TH1D(tree_label, branch_name, nbins, min_val, max_val);
+
+  double val;
+  int step;
+  tree->SetBranchAddress("step", &step);
+  tree->SetBranchAddress(branch_name, &val);
+
+  for(int i=0; i<tree->GetEntries(); i++){
+    tree->GetEntry(i);
+
+    if(step<step_cut){
+      continue;
+    }
+
+    if(val<min_val){
+      val = 2*max_val + val;
+    }
+    if(val>max_val){
+      val = 2*min_val + val;
+    }
+    if(step>100000){
+      h->Fill(val);
+    }
+
+  }
+  tree->ResetBranchAddresses();
+  return h;
+}
+
+TH1D* get_hist_from_ttree(TTree* tree, TString branch_name, TString tree_label, int nbins, double min_val, double max_val, bool nova=false){
   
-  TH1D* h = new TH1D(tree_label, "trace_comp:step"+branch_name, nbins, min_val, max_val);
-  tree->Draw(branch_name+">>"+tree_label, "step>100000");
+  TH1D* h;
+
+  TString cut = "(step>100000)";
+  if(nova){
+    // Apply nova cuts
+    // cut += "*(TMath::Cos(delta_cp))";
+  }
+
+  
+  if(nova && branch_name.Contains("delta_cp")){
+    // Fix the nova hist
+    h = fix_hist(tree, branch_name, tree_label, -1*TMath::Pi(), TMath::Pi(), nbins);
+  }
+
+  else{
+    h = new TH1D(tree_label, "trace_comp:step"+branch_name, nbins, min_val, max_val);
+    tree->Draw(branch_name+">>"+tree_label, cut, "goff");
+  }
+
   //  h->SetFillColorAlpha(kWhite, 0.0);
   h->Scale(1/static_cast<double>(tree->GetEntries()));
   return h;
 }
+
 // Simple script to compare traces
-void ComparePosteriors(TString file_1_name, TString file_1_lab, TString file_2_name, TString file_2_lab, TString output="posteriors_comp.pdf"){
+void compare_posteriors(TString file_1_name, TString file_1_lab, bool file_1_nova, TString file_2_name, TString file_2_lab, bool file_2_nova, TString output="posteriors_comp.pdf"){
   auto file_1_posteriors = get_posterior_tree(file_1_name);
   auto file_2_posteriors = get_posterior_tree(file_2_name);
 
@@ -48,8 +110,8 @@ void ComparePosteriors(TString file_1_name, TString file_1_lab, TString file_2_n
 
     int nbins = 50;
     
-    auto file_1_hist = get_hist_from_ttree(file_1_posteriors, branch_name, file_1_lab, nbins, min_val, max_val);
-    auto file_2_hist = get_hist_from_ttree(file_2_posteriors, branch_name, file_2_lab, nbins, min_val, max_val);
+    auto file_1_hist = get_hist_from_ttree(file_1_posteriors, branch_name, file_1_lab, nbins, min_val, max_val, file_1_nova);
+    auto file_2_hist = get_hist_from_ttree(file_2_posteriors, branch_name, file_2_lab, nbins, min_val, max_val, file_2_nova);
 
     TLegend* leg = new TLegend(0.7, 0.7, 0.9, 0.9);
     leg->AddEntry(file_1_hist, file_1_lab);
